b56f6f3b42b61bc596566c0502d22c82322ea347
furkandevren47@gmail.com <furkandevren47@gmail.com>
2024-12-06 01:38:22 +0300

    Add project files.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AppDbContext _context;
+
+        private readonly ProductRepository _productRepository;
+        public ProductsController(AppDbContext context)
+        {
+            //DI Container
+            //Dependency Injection Pattern
+            _productRepository = new ProductRepository();
+
+            _context = context;
+            //Linq method
+            if (!_context.Products.Any())
+            {
+                _context.Products.Add(new Product() { Name = "Kalem 1", Price = 100, Stock = 100, Color = "Red", Height = 10, Width = 20 });
+                _context.Products.Add(new Product() { Name = "Kalem 2", Price = 100, Stock = 200, Color = "Red", Height = 10, Width = 20 });
+                _context.Products.Add(new Product() { Name = "Kalem 3", Price = 100, Stock = 300, Color = "Red", Height = 10, Width = 20 });
+
+                _context.SaveChanges();
+            }
+
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+
+            var product = _context.Products.First();
+
+            return View(products);
+        }
+
+        public IActionResult Remove(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            _context.Products.Remove(product);
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string? Color { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+
+    }
+}
